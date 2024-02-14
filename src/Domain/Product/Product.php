8a070ca90220e260afe0c4e4888545a06c7a25c9
adriancecilia <adrian.cecilia-ogalla@akkodis.com>
2024-02-14 18:35:33 +0100

    feat(price): Added euro
    Added euro symbol on price data

--- src/Domain/Product/Product.php
+++ src/Domain/Product/Product.php
@@ -51,7 +51,7 @@ final class Product extends Model
 
     public function getPrice(): float
     {
-        return $this->getAttribute('price') . ' €';
+        return $this->getAttribute('price');
     }
 
     public function getStock(): int
